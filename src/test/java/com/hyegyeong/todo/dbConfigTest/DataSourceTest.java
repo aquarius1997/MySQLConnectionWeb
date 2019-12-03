f1f3493f1664ac057035deafe82f964d11f16548
송혜경 <>
2019-12-03 15:03:12 +0900

    First Commit

---
+++ src/test/java/com/hyegyeong/todo/dbConfigTest/DataSourceTest.java
@@ -0,0 +1,27 @@
+package com.hyegyeong.todo.dbConfigTest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.inject.Inject;
+import javax.sql.DataSource;
+import java.sql.Connection;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"file:web/WEB-INF/applicationContext.xml"})
+public class DataSourceTest {
+
+    @Inject
+    private DataSource dataSource;
+
+    @Test
+    public void testConnection() throws Exception {
+        try(Connection connection = dataSource.getConnection()) {
+            System.out.println(connection);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
